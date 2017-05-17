c121753d1576a57371872fead1aa0cc3fa17186d
1600571 <1600571@users.noreply.github.com>
2017-05-17 02:05:33 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
 sudo apt-get install -y git puppetmaster
+sudo puppet module install puppetlabs/windows
+cd /etc/puppet/modules
 git clone https://github.com/1600571/wintesti.git
